caa988f8e6c1f164412095624f3b48e1cd57e104
nixfox <dayvi1990@gmail.com>
2016-06-22 16:15:12 +0200

    initial commit

---
+++ rofi/rofi-run.sh
@@ -0,0 +1,6 @@
+rofi -show run -sidebar-mode -font "Dejavu Sans Mono 9" -location 3 -yoffset 30 -xoffset -5 -width 350 -color-window "#363636, argb:0ffffff, argb:0000000" -color-normal "argb:0fdf6e3, 
+#f5cccc, 
+argb:0eee8d5, 
+argb:f0f03333, 
+#ffe2e2" 
+-color-active "argb:0fdf6e3, #268bd2, argb:0eee8d5, argb:0268bd2, #fdf6e3" -color-urgent "argb:0fdf6e3, #dc322f, argb:0eee8d5, argb:0dc322f, #fdf6e3"
---
+++ rofi/rofi-ssh.sh
@@ -0,0 +1,1 @@
+rofi -bg "#222222" -bgalt "#232323" -fg "#eee" -hlbg "#BF2E2E" -font "monospace 11" -lines "10" -width "-30" -location "3" -bc "#222222" -show ssh
